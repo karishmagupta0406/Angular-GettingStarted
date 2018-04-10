91018bc67773b44aa1c0976f78a5cada393bd754
Karishma Gupta <guptakarishma04@gmail.com>
2018-04-10 23:44:35 +0530

    feature module

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -4,10 +4,20 @@
   selector: 'pm-root',
   template:`
   <div>
-    <h1>{{title}}</h1>
-    <pm-products></pm-products>
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <a class="navbar-brand">{{pageTitle}}</a>
+        <ul class="nav navbar-nav">
+          <li><a [routerLink]="['/welcome']">Home</a></li>
+          <li><a [routerLink]="['/products']">Product List</a></li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container">
+      <router-outlet></router-outlet>
+    </div>
   </div>`
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle = 'Angular: Getting Started';
 }
